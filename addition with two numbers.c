d4b53b50622b6f06b17b60c063eb8a47fae02e17
ranjith0123456789 <93901121+ranjith0123456789@users.noreply.github.com>
2021-11-25 16:28:01 +0530

    Create addition with two numbers.c

---
+++ addition with two numbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a,b,sum;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    sum=a+b;
+    printf("sum of two numbers:%d",sum);
+    return 0;
+}
